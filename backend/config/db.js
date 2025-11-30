8bdd65c4541946313e9bf595c3b20057156fcf4a
MaiTranNgocTran <tranb2207572@student.ctu.edu.vn>
2025-12-01 01:20:58 +0700

    update tính phạt trả sách trễ

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       // Sử dụng parser mới cho chuỗi kết nối
       useNewUrlParser: true,
-      // Sử dụng topology thống nhất để quản lý kết nối
+      // Sử dụng topology thống nhất để quản lý kết nối.
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
 };
